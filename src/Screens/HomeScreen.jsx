420f53b6dda1ea9691d0315ce121511c78150483
kuvii <kuvilet@gmail.com>
2023-02-09 23:12:19 +0000

    feat(HomeScreen): add screen skeleton base

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { getUserProfile } from '../api/api'
 import { useEffect } from 'react'
 
@@ -6,16 +6,33 @@ const HomeScreen = () => {
     useEffect(() => {
         const fetchUserProfile = async () => {
             const data = await getUserProfile()
-            console.log(data)
         }
         fetchUserProfile()
     }, [])
 
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.center}>
+        <View style={{...styles.stack, justifyContent: 'center'}}>
+        </View>
+        <View style={styles.stack}></View>
+        <View style={{...styles.stack, height: 200}}></View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    center: {
+        flex: 1,
+        paddingTop: 10,
+        alignItems: 'center'
+    },
+    stack: {
+        backgroundColor: 'lightgrey',
+        borderWidth: 1,
+        height: 100,
+        margin: 10,
+        width: 300,
+    },
+})
 
 export default HomeScreen
