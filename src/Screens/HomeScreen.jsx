d34f3032cead5a36da0d39c8eb6ea470af887a89
kuvii <kuvilet@gmail.com>
2023-02-09 22:10:33 +0000

    feat(HomeScreen): add fetchUserProfile funct

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,6 +1,16 @@
 import { View, Text } from 'react-native'
+import { getUserProfile } from '../api/api'
+import { useEffect } from 'react'
 
 const HomeScreen = () => {
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            const data = await getUserProfile()
+            console.log(data)
+        }
+        fetchUserProfile()
+    }, [])
+
   return (
     <View>
       <Text>HomeScreen</Text>
