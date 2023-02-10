5efd66f401624b8d2d3aaad51f004750900d2b54
kuvii <kuvilet@gmail.com>
2023-02-10 09:28:21 +0000

    feat(HomeScreen): add username and avatar

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -1,18 +1,40 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 import { getUserProfile } from '../api/api'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
+const initUserProfile = {
+    credits: 0,
+    joinedAt: '',
+    shipCount: 0,
+    structureCount: 0,
+    username: '',
+}
 
 const HomeScreen = () => {
+
+    const [userProfile, setUserProfile] = useState(initUserProfile)
+
     useEffect(() => {
         const fetchUserProfile = async () => {
             const data = await getUserProfile()
+            setUserProfile(data.user)
         }
         fetchUserProfile()
     }, [])
 
+    if (userProfile.username.length >= 18){
+        setUserProfile({...userProfile, username: (username.substring(0, 15) + '...')})
+    }
+
   return (
     <View style={styles.center}>
-        <View style={{...styles.stack, justifyContent: 'center'}}>
+        <View style={{...styles.stack, alignItems: 'center', flexDirection: 'row'}}>
+            <View style={styles.avatarContainer}>
+                <Image source={require('../../assets/avatars/slime_purple.png')} alt='avatar' style={{height: '90%', width: '80%'}} />
+            </View>
+            <View style={styles.username}>
+                <Text style={{fontSize: 20}}>{userProfile.username}</Text>
+            </View>
         </View>
         <View style={styles.stack}></View>
         <View style={{...styles.stack, height: 200}}></View>
@@ -33,6 +55,23 @@ const styles = StyleSheet.create({
         margin: 10,
         width: 300,
     },
+    avatarContainer: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: 'black',
+        borderColor: 'grey',
+        borderRadius: 100,
+        borderWidth: 2,
+        justifyContent: 'center',
+        marginLeft: 10,
+        height: '90%',
+    },
+    username: {
+        flex: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%'
+    }
 })
 
 export default HomeScreen
