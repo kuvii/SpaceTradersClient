b11fb7658c3d5c65346144d22e6f9d58bad7b337
kuvii <kuvilet@gmail.com>
2023-02-10 09:52:59 +0000

    refactor(images): set image resources as global

---
+++ src/utils/images.js
@@ -0,0 +1,8 @@
+export const images = {
+    avatars: {
+        purpleAvatar: require('../../assets/avatars/slime_purple.png'),
+    },
+    resources: {
+        credits: require('../../assets/resources/manat.png')
+    }
+}
