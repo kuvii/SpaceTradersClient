b87af8e78bc5f40bd792cace87a1135f219f0161
kuvii <kuvilet@gmail.com>
2023-02-10 10:57:46 +0000

    feat(images): add more avatars

--- src/utils/images.js
+++ src/utils/images.js
@@ -1,6 +1,8 @@
 export const images = {
     avatars: {
         purpleAvatar: require('../../assets/avatars/slime_purple.png'),
+        lighBlueAvatar: require('../../assets/avatars/slime_lightblue.png'),
+        greyAvatar: require('../../assets/avatars/slime_grey.png'),
     },
     resources: {
         credits: require('../../assets/resources/manat.png'),
@@ -8,4 +10,14 @@ export const images = {
         structures: require('../../assets/resources/structures.png'),
         joinedDate: require('../../assets/resources/calendar.png'),
     }
+}
+
+export const getRandomAvatar = () => {
+    const avatarsPositions = [
+        images.avatars.greyAvatar,
+        images.avatars.lighBlueAvatar,
+        images.avatars.greyAvatar
+    ]
+
+    return avatarsPositions[Math.floor(Math.random() * avatarsPositions.length)]
 }
