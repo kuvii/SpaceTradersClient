1222100c42efd3eb8a8d2c6ce0bfa42bf17efcfb
kuvii <kuvilet@gmail.com>
2023-02-13 22:09:43 +0000

    refactor(ShipsScreen): add fetch function

--- src/Screens/ShipsScreen.jsx
+++ src/Screens/ShipsScreen.jsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
+import { getSpaceShips } from '../api/api'
 
 const ShipsScreen = () => {
+
+  const [spaceshipList, setSpaceShipList] = useState([])
+
+  useEffect(() => {
+    const fetchGetShips = async () => {
+      try {
+        const data = await getSpaceShips()
+        setSpaceShipList(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchGetShips()
+  }, [])
+
   return (
     <View>
       <Text>ShipsScreen</Text>
