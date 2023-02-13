b2a15fa998377dcf26fc61c0c37c5ca396370691
kuvii <kuvilet@gmail.com>
2023-02-13 23:32:00 +0000

    feat(ShipsScreen): add ships list

--- src/Screens/ShipsScreen.jsx
+++ src/Screens/ShipsScreen.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { getSpaceShips } from '../api/api'
+import { FlatList } from 'react-native-gesture-handler'
+import ShipContainer from '../components/ShipContainer'
 
 const ShipsScreen = () => {
 
@@ -22,10 +24,39 @@ const ShipsScreen = () => {
   }, [])
 
   return (
-    <View>
-      <Text>ShipsScreen</Text>
+    <View style={styles.centerItems}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text style={{fontSize: 30, marginVertical: 20}}>Available ships</Text>
+      </View>
+      <View style={{flex: 5, height: '100%', width: '100%'}}>
+        <View style={styles.centerItems}>
+        <FlatList 
+          data={spaceShipList}
+          style= {{flex: 1, width: '70%'}}
+          renderItem={({item}) => {
+            const {type, speed, weapons, maxCargo} = item
+            return (
+              <View> 
+                <Text>{type}</Text>
+                <Text>{speed}</Text>
+                <Text>{weapons}</Text>
+                <Text>{maxCargo}</Text>
+              </View>
+              )
+            }}
+        />
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  centerItems: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
 
 export default ShipsScreen
