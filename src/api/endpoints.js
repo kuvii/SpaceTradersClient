b374b98215606ead7d513ae1a4493d9b2a1f5b2d
kuvii <kuvilet@gmail.com>
2023-02-14 16:08:02 +0000

    refactor(endpoint): add token var to endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,7 +1,7 @@
 import * as credentials from "./token.json"
 
 export const endpoints = {
-    userProfile: `https://api.spacetraders.io/my/account?token=${credentials.token}`,
+    userProfile: token => `https://api.spacetraders.io/my/account?token=${token}`,
     serverStatus: 'https://api.spacetraders.io/game/status',
     spaceshipList: `https://api.spacetraders.io/systems/OE/ship-listings?token=${credentials.token}`,
     loansList: `https://api.spacetraders.io/types/loans?token=${credentials.token}`,
