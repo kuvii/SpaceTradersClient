9672f1a03d466632e56b57eb0e5e01b603d4ba83
kuvii <kuvilet@gmail.com>
2023-02-14 13:58:24 +0000

    feat(LoginScreen): refactor input

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -5,6 +5,18 @@ import constants from '../secure/constants'
 const LoginScreen = ({onLogin}) => {
 
   const [inputToken, setInputToken] = useState('')
+  const [isIncorrect, setIsIncorrect] = useState(false)
+
+  const handleSubmit = () => {
+    if (inputToken.trim() != ''){
+      onLogin(constants.STORED_TOKEN_KEY, inputToken)
+    } else {
+      setIsIncorrect(true)
+      setTimeout(() => {
+        setIsIncorrect(false)
+      }, 2000)
+    }
+  }
 
   return (
     <View style={styles.centerItems}>
@@ -13,8 +25,9 @@ const LoginScreen = ({onLogin}) => {
       </View>
       <View style={{flex: 3, height: '100%', width: '100%', alignItems: 'center'}}>
         <View style={styles.loginForm}>
-          <TextInput value={inputToken} onChangeText={setInputToken} style={styles.inputToken}/>
-          <Button title='Login' onPress={() => onLogin(constants.STORED_TOKEN_KEY, inputToken)}/>
+          <TextInput value={inputToken} onChangeText={setInputToken} placeholder='Token' style={isIncorrect ? styles.incorrectInputToken : styles.correctInputToken}/>
+          {isIncorrect ? <Text style={{color: 'red'}}>Incorrect Token</Text> : <></>}
+          <Button title='Login' onPress={handleSubmit}/>
         </View>
       </View>
     </View>
@@ -27,9 +40,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  inputToken: {
+  correctInputToken: {
     backgroundColor: 'lightgrey',
-    width: 200
+    width: 260,
+    paddingHorizontal: 5,
+    margin: 5,
+  },
+  incorrectInputToken: {
+    backgroundColor: 'lightgrey',
+    borderColor: 'red',
+    borderWidth: 1,
+    width: 260,
+    paddingHorizontal: 5,
+    margin: 5,
   },
   loginForm: {
     alignItems: 'center',
