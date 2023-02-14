b75da0c899c49ca4184bcf53fe34f34246a305a2
kuvii <kuvilet@gmail.com>
2023-02-14 01:01:04 +0000

    feat(LoansScreen): implement loans service

--- src/Screens/LoansScreen.jsx
+++ src/Screens/LoansScreen.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from 'react'
 import { View, Text } from 'react-native'
+import { getLoansAvailable } from '../api/api'
 
 const LoansScreen = () => {
+
+  const [loansAvailable, setLoansAvailable] = useState([])
+
+  useEffect(() => {
+    const fetchGetLoansAvailable = async () => {
+      try {
+        const data = await getLoansAvailable()
+        const loans = data.loans
+        if (loansAvailable.length == 0){
+          setLoansAvailable(loans)
+        }
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchGetLoansAvailable()
+  }, [])
+
   return (
     <View>
       <Text>LoansScreen</Text>
