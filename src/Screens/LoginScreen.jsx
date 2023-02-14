a0ae2706f05c99e0f181a52f44d0ba29a53ffc53
kuvii <kuvilet@gmail.com>
2023-02-14 12:22:58 +0000

    feat(LoginScreen): add login screen

--- src/Screens/LoginScreen.jsx
+++ src/Screens/LoginScreen.jsx
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
+import { useState } from 'react'
+import constants from '../secure/constants'
 
-const LoginScreen = () => {
+const LoginScreen = ({onLogin, setUserToken}) => {
+
+  const [inputToken, setInputToken] = useState('')
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.centerItems}>
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Text style={{fontSize: 30, marginVertical: 20}}>Login</Text>
+      </View>
+      <View style={{flex: 3, height: '100%', width: '100%', alignItems: 'center'}}>
+        <View style={styles.loginForm}>
+          <TextInput value={inputToken} onChangeText={setInputToken} style={styles.inputToken}/>
+          <Button title='Login' onPress={() => onLogin(setUserToken, constants.STORED_TOKEN_KEY, inputToken)}/>
+        </View>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  centerItems: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputToken: {
+    backgroundColor: 'lightgrey',
+    width: 200
+  },
+  loginForm: {
+    alignItems: 'center',
+    flex: 2, 
+    height: 200, 
+    justifyContent: 'flex-start',
+    width: 200, 
+  },
+})
 
 export default LoginScreen
