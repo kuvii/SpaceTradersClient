615d96743cb043af95c62f4a47d216189badc14e
kuvii <kuvilet@gmail.com>
2023-02-14 17:45:44 +0000

    refactor(endpoints): remove credentials import

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,10 +1,9 @@
-import * as credentials from "./token.json"
 
 export const endpoints = {
     userProfile: `https://api.spacetraders.io/my/account?token=`,
     serverStatus: 'https://api.spacetraders.io/game/status',
-    spaceshipList: `https://api.spacetraders.io/systems/OE/ship-listings?token=${credentials.token}`,
-    loansList: `https://api.spacetraders.io/types/loans?token=${credentials.token}`,
-    takeOutLoan: `https://api.spacetraders.io/my/loans/?token=${credentials.token}`,
+    spaceshipList: `https://api.spacetraders.io/systems/OE/ship-listings?token=`,
+    loansList: `https://api.spacetraders.io/types/loans?token=`,
+    takeOutLoan: `https://api.spacetraders.io/my/loans/?token=`,
     createUser: user => `https://api.spacetraders.io/users/${user}/claim`,
 }
